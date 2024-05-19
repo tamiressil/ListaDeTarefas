f378b329da664e0bb9200010ee393c7cdeabecce
tamiressil <tamiressilvabezerra05@gmail.com>
2024-05-19 16:06:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ListaDeTarefas from "./Componentes/ListaTarefas";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ListaDeTarefas />
+    </div>
+  );
+}
